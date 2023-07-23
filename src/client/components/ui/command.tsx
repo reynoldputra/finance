5b69784398a00d9be3ad848f188c1495fe398dac
reynoldputra <reynoldputra1@gmail.com>
2023-07-24 00:10:59 +0700

    rebase: master

--- src/client/components/ui/command.tsx
+++ src/client/components/ui/command.tsx
@@ -2,7 +2,6 @@ import * as React from "react"
 import { DialogProps } from "@radix-ui/react-dialog"
 import { Command as CommandPrimitive } from "cmdk"
 import { Search } from "lucide-react"
-
 import { cn } from "@client/lib/cn"
 import { Dialog, DialogContent } from "@client/components/ui/dialog"
 
--- src/client/components/ui/sheet.tsx
+++ src/client/components/ui/sheet.tsx
@@ -2,7 +2,6 @@ import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
-
 import { cn } from "@client/lib/cn"
 
 const Sheet = SheetPrimitive.Root
@@ -35,7 +34,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
 
 const sheetVariants = cva(
-  "fixed z-50 gap-4 bg-white p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
   {
     variants: {
       side: {

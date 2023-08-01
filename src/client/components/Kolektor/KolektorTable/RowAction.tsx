22c663efe6abc03cbf923f5f671132387f907c28
treeoon <wisnuyeahfaizal@gmail.com>
2023-08-02 01:38:43 +0700

    feat: delete collector action

--- src/client/components/Kolektor/KolektorTable/RowAction.tsx
+++ src/client/components/Kolektor/KolektorTable/RowAction.tsx
@@ -1,27 +1,44 @@
 import {
-    DropdownMenuItem,
-    DropdownMenuSeparator,
-    DropdownMenuShortcut,
-  } from "@client/components/ui/dropdown-menu"
-  import { Row } from "@tanstack/react-table"
-  import { DataTableRowActions } from "@client/components/table/DataTableRowActions"
-  
-  interface RowActionsProps<TData> {
-    row: Row<TData>
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+} from "@client/components/ui/dropdown-menu";
+import { Row } from "@tanstack/react-table";
+import { DataTableRowActions } from "@client/components/table/DataTableRowActions";
+import { TKolektorTable } from "../../../../server/collections/kolektor/kolektorSchema";
+import { trpc } from "@client/lib/trpc";
+
+interface RowActionsProps<TData> {
+  row: Row<TData>;
+}
+
+export function RowAction({ row }: RowActionsProps<TKolektorTable>) {
+  const deleteKolektorMutation = trpc.kolektor.deleteKolektor.useMutation();
+  async function handleDelete() {
+    try {
+      const { data } = await deleteKolektorMutation.mutateAsync(
+        row.original.id
+      );
+      if (data) {
+        console.log("Kolektor berhasil dihapus:", data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
-  
-  export function RowAction<TData>({
-    row,
-  }: RowActionsProps<TData>) {
-    return (
-      <DataTableRowActions>
-        <DropdownMenuItem>Edit</DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          Delete
-          <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DataTableRowActions>
-    );
-  }
-  
+  return (
+    <DataTableRowActions>
+      <DropdownMenuItem>Edit</DropdownMenuItem>
+      <DropdownMenuItem
+        onClick={() => navigator.clipboard.writeText(row.original.id)}
+      >
+        Copy Kolektor ID
+      </DropdownMenuItem>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem onClick={handleDelete}>
+        Delete
+        <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
+      </DropdownMenuItem>
+    </DataTableRowActions>
+  );
+}

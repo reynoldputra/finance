bb95f82c73a4b55f5db9a4983d084a833e77cae7
reynoldputra <reynoldputra1@gmail.com>
2023-08-23 10:37:41 +0700

    feat: header report bank

--- src/client/components/Home/Home.tsx
+++ src/client/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import ReportSetoranBank from "../report/reportSetoranBank";
 
 export interface HomeProps {}
 
-export const Home: React.FC<HomeProps> = (props) => {
+export const Home: React.FC<HomeProps> = () => {
   return (
     <div className="w-full min-h-screen flex flex-col p-10">
       <h1 className="font-bold text-3xl">Dashboard</h1>
+      <ReportSetoranBank />
     </div>
   );
 };
